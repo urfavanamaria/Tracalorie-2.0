1856112249a74119757b02905fba0df80d618a36
Anamaria <natia.anamaria@gmail.com>
2025-08-06 15:23:50 +0400

    Set claorie limit

--- js/app.js
+++ js/app.js
@@ -52,6 +52,12 @@ class CalorieTracker {
     this._render();
   }
 
+  setLimit(calorieLimit) {
+    this._calorieLimit = calorieLimit;
+    this._displayCaloriesLimit();
+    this._render();
+  }
+
   // private methods
   _displayCaloriesTotal() {
     const totalCaloriesEl = document.getElementById('calories-total');
@@ -202,6 +208,9 @@ class App {
     document
       .getElementById('reset')
       .addEventListener('click', this._reset.bind(this));
+    document
+      .getElementById('limit-form')
+      .addEventListener('submit', this._setLimit.bind(this));
   }
 
   _newItem(type, e) {
@@ -269,6 +278,23 @@ class App {
     document.getElementById('workout-items').innerHTML = '';
     document.getElementById('filter-meals').value = '';
     document.getElementById('filter-workouts').value = '';
+  }
+
+  _setLimit(e) {
+    e.preventDefault();
+    const limit = document.getElementById('limit');
+
+    if (limit.value === '') {
+      alert('Please enter a valid calorie limit');
+      return;
+    }
+
+    this._tracker.setLimit(+limit.value);
+    limit.value = '';
+
+    const modalEl = document.getElementById('limit-modal');
+    const modal = bootstrap.Modal.getInstance(modalEl);
+    modal.hide();
   }
 }
 
